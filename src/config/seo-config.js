eb75d9cda0662e2a3ad0f1fe80212d8e18840a0b
ru4s93m6 <ru4s93m6@yahoo.com.tw>
2026-05-10 17:11:24 +0800

    feat: adjust SEO content

--- src/config/seo-config.js
+++ src/config/seo-config.js
@@ -4,7 +4,7 @@
 export const homePageSEO = {
   'zh-tw': {
     title: '尼泊爾當地旅行社 | Oriental Express Travel 彈性包團・深度旅遊',
-    description: 'Oriental Express Travel 是一間深耕尼泊爾的當地旅行社。我們專注於彈性的私人客製化包團，無論人數多寡、隨時都能出發。無論是輕鬆的古都文化巡禮、野生動物生態遊，還是挑戰自我的高山健行，交給專業的團隊，帶你深度體驗最真實的尼泊爾。',
+    description: 'Oriental Express Travel 是一間深耕尼泊爾的當地旅行社。我們專注於彈性的私人客製化包團，無論人數多寡、隨時都能出發。不管是輕鬆的古都文化巡禮、野生動物生態遊，還是挑戰自我的高山健行，交給專業的團隊，帶你深度體驗最真實的尼泊爾。',
     keywords: '尼泊爾旅遊,尼泊爾健行,尼泊爾包團,尼泊爾導遊,聖母峰基地營,安納普納健行,加德滿都旅遊,尼泊爾旅行社,尼泊爾當地旅行社,尼泊爾在地旅行社,Oriental Express Travel,Nepal Trekking',
   },
   'en': {
